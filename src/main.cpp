c00116ace4cdc0e6bcc1aace94f3f4a18d58a942
Connor Braa <connor.braa@gmail.com>
2013-11-18 17:36:16 -0800

    added comment outline to main

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+
+
+int main(int argc, char** argv)
+{
+    //parse input file, numParticles, output file from argv
+
+    //open input file, find dimensions
+
+    //initialize particles based on input dimensions
+
+    while (video not over) {
+        //open evidenceFrame
+        //generate emission model : P(evidenceFrame|each location)
+        //observe and resample using emissionmodel
+        //elapse time : move each particle based on transition model : P(PART'| part)
+        //output particles
+        //move evidenceFrame pointer
+    }
+
+    //free memory
+}
